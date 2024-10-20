653fa06c5df0fd05b2aa3e4ade4f4a6c9d8297c0
Aymaan-Shabbir <aymaanshabbir27@gmail.com>
2024-10-20 15:08:40 +0530

    updated search field

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -18,7 +18,7 @@ export default function Page({ searchParams: { q } }: PageProps) {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <div className="rounded-2xl bg-card p-5 shadow-sm">
-          <h1 className="line-clamp-2 break-all text-center text-2xl font-bold">
+          <h1 className="line-clamp-2 break-all text-center text-lg font-bold">
             Search results for &quot;{q}&quot;
           </h1>
         </div>
